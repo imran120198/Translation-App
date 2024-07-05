7a6e3bd870dbbbaec4f72e89d72bf0810a717a4c
Imran Khan <99397606+imran120198@users.noreply.github.com>
2024-07-05 09:59:44 +0530

    navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../Styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+      <h1>Language Translator</h1>
+    </div>
+  );
+};
+
+export default Navbar;
