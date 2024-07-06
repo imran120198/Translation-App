1a5c48d27ddc7dfdfb127e89af8480aa3c70edaa
Imran Khan <99397606+imran120198@users.noreply.github.com>
2024-07-06 09:44:10 +0530

    translator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import Translator from "./Components/Translator";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Translator />
+      <ToastContainer />
     </div>
   );
 }
